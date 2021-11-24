276c773cd8b4e139ff49335ee154d2e06fa93617
WANNA959 <wannazjx@163.com>
2021-11-24 21:33:47 +0800

    支持tar whl解压缩

--- utils/fileUtil.go
+++ utils/fileUtil.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"archive/tar"
 	"archive/zip"
+	"compress/gzip"
 	uuid "github.com/satori/go.uuid"
 	"io"
 	"os"
@@ -114,6 +116,88 @@ func Unzip(zipFile string, destDir string) error {
 	return nil
 }
 
+func Untargz(src, dst string) (err error) {
+	// 打开准备解压的 tar 包
+	fr, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer fr.Close()
+
+	// 将打开的文件先解压
+	gr, err := gzip.NewReader(fr)
+	if err != nil {
+		return
+	}
+	defer gr.Close()
+
+	// 通过 gr 创建 tar.Reader
+	tr := tar.NewReader(gr)
+
+	// 现在已经获得了 tar.Reader 结构了，只需要循环里面的数据写入文件就可以了
+	for {
+		hdr, err := tr.Next()
+
+		switch {
+		case err == io.EOF:
+			return nil
+		case err != nil:
+			return err
+		case hdr == nil:
+			continue
+		}
+
+		// 处理下保存路径，将要保存的目录加上 header 中的 Name
+		// 这个变量保存的有可能是目录，有可能是文件，所以就叫 FileDir 了……
+		dstFileDir := filepath.Join(dst, hdr.Name)
+
+		// 根据 header 的 Typeflag 字段，判断文件的类型
+		switch hdr.Typeflag {
+		case tar.TypeDir: // 如果是目录时候，创建目录
+			// 判断下目录是否存在，不存在就创建
+			if b := ExistDir(dstFileDir); !b {
+				// 使用 MkdirAll 不使用 Mkdir ，就类似 Linux 终端下的 mkdir -p，
+				// 可以递归创建每一级目录
+				if err := os.MkdirAll(dstFileDir, 0775); err != nil {
+					return err
+				}
+			}
+		case tar.TypeReg: // 如果是文件就写入到磁盘
+			// 创建一个可以读写的文件，权限就使用 header 中记录的权限
+			// 因为操作系统的 FileMode 是 int32 类型的，hdr 中的是 int64，所以转换下
+			file, err := os.OpenFile(dstFileDir, os.O_CREATE|os.O_RDWR, os.FileMode(hdr.Mode))
+			if err != nil {
+				return err
+			}
+			_, err = io.Copy(file, tr)
+			if err != nil {
+				return err
+			}
+			// 将解压结果输出显示
+			//fmt.Printf("成功解压： %s , 共处理了 %d 个字符\n", dstFileDir, n)
+
+			// 不要忘记关闭打开的文件，因为它是在 for 循环中，不能使用 defer
+			// 如果想使用 defer 就放在一个单独的函数中
+			file.Close()
+		}
+	}
+
+	return nil
+}
+
+//解压缩.whl
+func UnWheel(src, dst string) error {
+	src = strings.ReplaceAll(src, ".whl", ".zip")
+	os.Rename(src, dst)
+	return Unzip(src, dst)
+}
+
+// 判断目录是否存在
+func ExistDir(dirname string) bool {
+	fi, err := os.Stat(dirname)
+	return (err == nil || os.IsExist(err)) && fi.IsDir()
+}
+
 //只含单文件的文件夹中 判断文件类型
 func GetSingleFileName(filePath string) string {
 	filepathNames, _ := filepath.Glob(filepath.Join(filePath, "*"))
@@ -131,96 +215,6 @@ func JudgeSingleFileType(filePath string) string {
 	idx := strings.LastIndex(fileName, ".")
 	fileType := fileName[idx+1:]
 	return fileType
-}
-
-//压缩文件
-//files 文件数组，可以是不同dir下的文件或者文件夹
-//dest 压缩文件存放地址
-func Compress(files []*os.File, dest string) error {
-	d, _ := os.Create(dest)
-	defer d.Close()
-	w := zip.NewWriter(d)
-	defer w.Close()
-	for _, file := range files {
-		err := compress(file, "", w)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func compress(file *os.File, prefix string, zw *zip.Writer) error {
-	info, err := file.Stat()
-	if err != nil {
-		return err
-	}
-	if info.IsDir() {
-		prefix = prefix + "/" + info.Name()
-		fileInfos, err := file.Readdir(-1)
-		if err != nil {
-			return err
-		}
-		for _, fi := range fileInfos {
-			f, err := os.Open(file.Name() + "/" + fi.Name())
-			if err != nil {
-				return err
-			}
-			err = compress(f, prefix, zw)
-			if err != nil {
-				return err
-			}
-		}
-	} else {
-		header, err := zip.FileInfoHeader(info)
-		header.Name = prefix + "/" + header.Name
-		if err != nil {
-			return err
-		}
-		writer, err := zw.CreateHeader(header)
-		if err != nil {
-			return err
-		}
-		_, err = io.Copy(writer, file)
-		file.Close()
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-//解压
-func DeCompress(zipFile, dest string) error {
-	reader, err := zip.OpenReader(zipFile)
-	if err != nil {
-		return err
-	}
-	defer reader.Close()
-	for _, file := range reader.File {
-		rc, err := file.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-		filename := dest + file.Name
-		err = os.MkdirAll(getDir(filename), 0755)
-		if err != nil {
-			return err
-		}
-		w, err := os.Create(filename)
-		if err != nil {
-			return err
-		}
-		defer w.Close()
-		_, err = io.Copy(w, rc)
-		if err != nil {
-			return err
-		}
-		w.Close()
-		rc.Close()
-	}
-	return nil
 }
 
 func getDir(path string) string {
--- utils/util_test.go
+++ utils/util_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"testing"
 )
@@ -10,18 +11,18 @@ func TestGetUUID(t *testing.T) {
 	fmt.Println(GetUUID())
 }
 
-func TestCompress(t *testing.T) {
-	pwd, _ := os.Getwd()
-	pwd = GetParentDirectory(pwd)
-	dirName := pwd + "/data"
-	filePath := fmt.Sprintf("%s/%s.zip", dirName, "numpy")
-	println(dirName)
-	println(filePath)
-	file, _ := os.Open(dirName + "/numpy")
-	defer file.Close()
-	var files = []*os.File{file}
-	Compress(files, filePath)
-}
+//func TestCompress(t *testing.T) {
+//	pwd, _ := os.Getwd()
+//	pwd = GetParentDirectory(pwd)
+//	dirName := pwd + "/data"
+//	filePath := fmt.Sprintf("%s/%s.zip", dirName, "numpy")
+//	println(dirName)
+//	println(filePath)
+//	file, _ := os.Open(dirName + "/numpy")
+//	defer file.Close()
+//	var files = []*os.File{file}
+//	//Compress(files, filePath)
+//}
 
 func TestZip(t *testing.T) {
 	pwd, _ := os.Getwd()
@@ -43,4 +44,33 @@ func TestJudgeFileType(t *testing.T) {
 	println(dirName)
 	fileName := JudgeSingleFileType(dirName)
 	println(fileName)
+}
+
+func TestUntargz(t *testing.T) {
+	srcPath := "/Users/zhujianxing/GoLandProjects/code/MS_RemoteCode/internal/data/panda-0.3.1.tar.gz"
+	destPath := "/Users/zhujianxing/GoLandProjects/code/MS_RemoteCode/internal/data/"
+	err := Untargz(srcPath, destPath)
+	log.Printf("err=%+v", err)
+}
+
+func TestUnzip(t *testing.T) {
+	srcPath := "/Users/zhujianxing/GoLandProjects/code/MS_RemoteCode/internal/data/numpy-1.19.3.zip"
+	destPath := "/Users/zhujianxing/GoLandProjects/code/MS_RemoteCode/internal/data/"
+	err := Unzip(srcPath, destPath)
+	log.Printf("err=%+v", err)
+}
+
+func TestUnzip2(t *testing.T) {
+	//.whl to .zip
+	srcPath := "/Users/zhujianxing/GoLandProjects/code/MS_RemoteCode/internal/data/numpy-1.21.4-cp39-cp39-macosx_11_0_arm64.zip"
+	destPath := "/Users/zhujianxing/GoLandProjects/code/MS_RemoteCode/internal/data/numpywheel/"
+	err := Unzip(srcPath, destPath)
+	log.Printf("err=%+v", err)
+}
+
+func TestUnWhell(t *testing.T) {
+	srcPath := "/Users/zhujianxing/GoLandProjects/code/MS_RemoteCode/internal/data/numpy-1.21.4-cp39-cp39-macosx_11_0_arm64.whl"
+	destPath := "/Users/zhujianxing/GoLandProjects/code/MS_RemoteCode/internal/data/numpywheel/"
+	err := UnWheel(srcPath, destPath)
+	log.Printf("err=%+v", err)
 }
